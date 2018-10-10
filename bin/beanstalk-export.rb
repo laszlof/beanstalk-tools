125ce66beb8055a735deea067542cc80d1f80df8
Frank Laszlo <flaszlo@nexcess.net>
2018-10-10 12:07:59 -0400

    Fixes

--- bin/beanstalk-export.rb
+++ bin/beanstalk-export.rb
@@ -32,7 +32,6 @@ def export(t)
                     :body => job.body)
     $stdout.flush
     $stderr.puts "Got job #{job.id} (+#{job.delay})"
-    # job.delete
   end
 rescue Beanstalk::TimedOut
   nil
